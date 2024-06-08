34ada13107e192288c25ef04f8a5cb8068319bc0
Prashanth Naroju <prashanth.naro@gmail.com>
2024-06-08 13:46:23 +0530

    add the comments to createserver code for more meaningful commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //this is the main root file for creating our node js server
+
+//code use the http core module to create the server for request and response from the server
 const http =require('http')
+
+//createServer() is the method which will take a callback function which will start taking the request from the clients into server
 
 const server=http.createServer((req,res)=>{
 
@@ -7,4 +11,5 @@ const server=http.createServer((req,res)=>{
     console.log(req)
 })
 
+//Listen() method is used to set up the port for server to launch and be in state of listening request from the clients
 server.listen(3000)
