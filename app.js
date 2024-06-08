367602499d7de28581f13c131e64d9b48aa309e7
Prashanth Naroju <prashanth.naro@gmail.com>
2024-06-08 13:43:21 +0530

    lesson 1 - use core modules to connect to server using http

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //this is the main root file for creating our node js server
+const http =require('http')
+
+const server=http.createServer((req,res)=>{
+
+    console.log("server created and ready to listen the client request")
+    console.log(req)
+})
+
+server.listen(3000)
