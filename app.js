999a735dfdb6d46b98b543f13938fbc207323b33
Prashanth Naroju <prashanth.naro@gmail.com>
2024-06-17 13:55:13 +0530

    import the routes requestHandler into app.js and run the server by using it to create server

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const routes=require('./routes') //import routes module so we can call the reque
 
 //createServer() is the method which will take a callback function which will start taking the request from the clients into server
 
-const server=http.createServer()
+const server=http.createServer(routes.requestHandler) //using the requesthandler function from routes modules
 
 //Listen() method is used to set up the port for server to launch and be in state of listening request from the clients
 server.listen(3000)
